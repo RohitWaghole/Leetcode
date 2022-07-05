b3e5332859523f994e4a8ca9d32c74e07f24450a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-05 22:48:00 +0530

    Added solution - LeetHub

--- Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.py
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.py
@@ -3,19 +3,7 @@
 class Solution:
     #Function to check if two arrays are equal or not.
     def check(self,A,B,N):
-        d1 = {}
-        d2 = {}
-        for i in A:
-            if i in d1:
-                d1[i]+=1
-            else:
-                d1[i]=1
-        for i in B:
-            if i in d2:
-                d2[i]+=1
-            else:
-                d2[i]=1
-        return d1==d2
+        return sorted(A)==sorted(B)
         #return: True or False
         
         #code here

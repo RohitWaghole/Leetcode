50e5f327c1b793cf868b5fe0717c2c3273332b46
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-05 12:49:33 +0530

    Update 1-two-sum.py

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,18 @@
+# T.C. -> O(N^2)
+# S.C. -> O(1)
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i]+nums[j]==target:
+#                     return [i,j]
+
+#######################################################################################################################################################################
+
+# T.C. -> O(N)
+# S.C. -> O(N)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
         d = {}
         for i,v in enumerate(nums):
             c = target-v

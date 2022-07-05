e990d67852ab6065b71e3dc9424491312a5bfcad
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 01:03:10 +0530

    Added solution - LeetHub

--- Missing number in array - GFG/missing-number-in-array.py
+++ Missing number in array - GFG/missing-number-in-array.py
@@ -4,8 +4,12 @@
 class Solution:
     def MissingNumber(self,array,n):
         # code here
-        c = n*(n+1)//2
-        return c-sum(array)
+        x = 0
+        for i in array:
+            x^=i
+        for i in range(1,n+1):
+            x^=i
+        return x
         
 
 #{ 

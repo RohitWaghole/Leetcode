54b6dbd38c6dbfb588ff2a4ca416b7baee167e82
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 09:40:16 +0530

    Time: 1107 ms (14.52%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -9,15 +9,27 @@
 
 #####################################################################################################
 
+# class Solution:
+#     def __init__(self):
+#         self.l=[]
+#         a,b=0,1
+#         for i in range(31):
+#             self.l.append(a)
+#             c = a+b
+#             a = b
+#             b = c
+#     def fib(self, n: int) -> int:
+#         return self.l[n]
+    
+#######################################################################################################
+
 class Solution:
-    def __init__(self):
-        self.l=[]
-        a,b=0,1
-        for i in range(31):
-            self.l.append(a)
-            c = a+b
-            a = b
-            b = c
-            
     def fib(self, n: int) -> int:
-        return self.l[n]
+        return self.getfib(n)
+    
+    def getfib(self,n):
+        
+        if n<=1:
+            return n
+        
+        return self.getfib(n-1) + self.getfib(n-2)

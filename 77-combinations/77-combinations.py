14ebd6c4a9972c99e714dc814bf0df2b52a54bcf
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 12:02:16 +0530

    Time: 843 ms (15.23%), Space: 16 MB (51.22%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,25 +1,25 @@
-# class Solution:
-#     def combine(self, n: int, k: int) -> List[List[int]]:
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
         
-#         l = [i for i in range(1,n+1)]
-#         def f(i,k,l,ans,ds):
+        l = [i for i in range(1,n+1)]
+        def f(i,k,l,ans,ds):
             
-#             if i>=len(l):
-#                 if len(ds)==k:
-#                     ans.append(ds[:])
-#                 return
-#             ds.append(l[i])
-#             f(i+1,k,l,ans,ds)
-#             ds.pop()
-#             f(i+1,k,l,ans,ds)
+            if i>=len(l):
+                if len(ds)==k:
+                    ans.append(ds[:])
+                return
+            ds.append(l[i])
+            f(i+1,k,l,ans,ds)
+            ds.pop()
+            f(i+1,k,l,ans,ds)
         
-#         ans = []
-#         f(0,k,l,ans,[])
-#         return ans
+        ans = []
+        f(0,k,l,ans,[])
+        return ans
 
 ###################################################################################################
 
-from itertools import combinations
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        return combinations([i for i in range(1,n+1)],k)
+# from itertools import combinations
+# class Solution:
+#     def combine(self, n: int, k: int) -> List[List[int]]:
+#         return combinations([i for i in range(1,n+1)],k)

a3ef7229fc9b749a4ddfab5fef620976f8affb2f
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 11:46:47 +0530

    Time: 197 ms (15.94%), Space: 14.2 MB (25.40%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        self.ans = []
+        def f(i,ds,target,candidates):
+            if target<0:
+                return
+            if i>=len(candidates):
+                if target==0:
+                    self.ans.append(ds[:])
+                return
+            ds.append(candidates[i])
+            f(i,ds,target-candidates[i],candidates)
+            ds.pop()
+            f(i+1,ds,target,candidates)
+            
+        f(0,[],target,candidates)
+        return self.ans

0dd9c532f8016cdf5d84af2afb951187af2a0b7e
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 11:59:27 +0530

    Time: 948 ms (9.58%), Space: 15.9 MB (85.01%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        l = [i for i in range(1,n+1)]
+        
+        def f(i,k,l,ans,ds):
+            
+            if i>=len(l):
+                if len(ds)==k:
+                    ans.append(ds[:])
+                return
+            ds.append(l[i])
+            f(i+1,k,l,ans,ds)
+            ds.pop()
+            f(i+1,k,l,ans,ds)
+        
+        ans = []
+        f(0,k,l,ans,[])
+        return ans

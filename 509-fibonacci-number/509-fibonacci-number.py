112d1bb200ca44bac4a5e5241a19bc1823a52951
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-06 09:33:15 +0530

    Time: 60 ms (37.85%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,23 @@
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         a,b=0,1
+#         for i in range(n):
+#             c = a+b
+#             a = b
+#             b = c
+#         return a
+
+#####################################################################################################
+
 class Solution:
-    def fib(self, n: int) -> int:
+    def __init__(self):
+        self.l=[]
         a,b=0,1
-        for i in range(n):
+        for i in range(31):
+            self.l.append(a)
             c = a+b
             a = b
             b = c
-        return a
+            
+    def fib(self, n: int) -> int:
+        return self.l[n]

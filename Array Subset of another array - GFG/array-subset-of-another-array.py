e2548005321574b38294c9cbc0843d3c27da7b57
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-07 23:48:37 +0530

    Added solution - LeetHub

--- Array Subset of another array - GFG/array-subset-of-another-array.py
+++ Array Subset of another array - GFG/array-subset-of-another-array.py
@@ -1,66 +1,78 @@
 #User function Template for python3
 
+# def isSubset( a1, a2, n, m):
+    
+#     for i in range(m):
+#         element = a2[i]
+#         flag = 1
+#         for j in range(n):
+#             if a1[j]==element:
+#                 flag = 0
+#                 break
+#         if flag:
+#             return "No"
+#     return "Yes"
+    
+# ############################################################################
+
+# def isSubset( a1, a2, n, m):
+#     a1.sort()
+#     for i in a2:
+#         if not binarySearch(a1,i):
+#             return "No"
+#     return "Yes"
+    
+# def binarySearch(lst,element):
+    
+#     start,end = 0, len(lst)-1
+#     while start<=end:
+#         mid = start+(end-start)//2
+#         if lst[mid]==element:
+#             return 1
+#         if lst[mid]>element:
+#             end = mid-1
+#         else:
+#             start = mid+1
+#     return 0
+
+####################################################################################
+
 def isSubset( a1, a2, n, m):
-    
-    # for i in range(m):
-    #     element = a2[i]
-    #     flag = 1
-    #     for j in range(n):
-    #         if a1[j]==element:
-    #             flag = 0
-    #             break
-    #     if flag:
-    #         return "No"
-    # return "Yes"
-    
-    
     a1.sort()
-    for i in a2:
-        if not binarySearch(a1,i):
-            return "No"
-    return "Yes"
-    
-def binarySearch(lst,element):
-    
-    start,end = 0, len(lst)-1
-    while start<=end:
-        mid = start+(end-start)//2
-        if lst[mid]==element:
-            return 1
-        if lst[mid]>element:
-            end = mid-1
+    a2.sort()
+    i,j=0,0
+    while i<n and j<m:
+        if a1[i]==a2[j]:
+            i+=1
+            j+=1
         else:
-            start = mid+1
-    return 0
+            i+=1
+    if j==m:
+        return "Yes"
+    return "No"
     
+# ##################################################################################
+
+# def isSubset( a1, a2, n, m):  
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # d1 = {}
-    # d2 = {}
-    # for i in a1:
-    #     if i in d1:
-    #         d1[i]+=1
-    #     else:
-    #         d1[i]=1
-    # for i in a2:
-    #     if i in d2:
-    #         d2[i]+=1
-    #     else:
-    #         d2[i]=1
+#     d1 = {}
+#     d2 = {}
+#     for i in a1:
+#         if i in d1:
+#             d1[i]+=1
+#         else:
+#             d1[i]=1
+#     for i in a2:
+#         if i in d2:
+#             d2[i]+=1
+#         else:
+#             d2[i]=1
             
-    # for key,val in d2.items():
-    #     c = d1.get(key,0)
-    #     if c<val:
-    #         return "No"
-    # return "Yes"
+#     for key,val in d2.items():
+#         c = d1.get(key,0)
+#         if c<val:
+#             return "No"
+#     return "Yes"
     
 
 

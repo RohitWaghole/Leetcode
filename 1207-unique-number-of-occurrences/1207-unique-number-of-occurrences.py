3f3643a03bfbdae19ae405f4ba0ef60223441199
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-08 08:45:25 +0530

    Time: 35 ms (96.22%), Space: 13.9 MB (97.52%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,10 +1,22 @@
+# class Solution:
+#     def uniqueOccurrences(self, arr: List[int]) -> bool:
+        
+#         d = {}
+#         for i in arr:
+#             if i in d:
+#                 d[i]+=1
+#             else:
+#                 d[i]=1
+#         return len(d.values())==len(set(d.values()))
+
+#####################################################################################
+
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         
-        d = {}
-        for i in arr:
-            if i in d:
-                d[i]+=1
-            else:
-                d[i]=1
-        return len(d.values())==len(set(d.values()))
+        seen = set()
+        for frequency in Counter(arr).values():
+            if frequency in seen:
+                return False
+            seen.add(frequency)
+        return True

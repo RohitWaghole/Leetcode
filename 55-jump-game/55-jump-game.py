c851ae5a5e8c31b0c41c7206cb0f6e1992865f85
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-09 15:38:51 +0530

    Time: 1137 ms (14.31%), Space: 15.1 MB (96.76%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,9 +1,22 @@
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+        
+#         # TRAVERSE FROM BACKWARDS UNTIL GOAL BECOMES FIRST INDEX
+#         goal = len(nums)-1
+        
+#         for i in range(len(nums)-1,-1,-1):
+#             if i+nums[i]>=goal:
+#                 goal = i
+#         return goal==0
+
+#############################################################################################################
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
-        goal = len(nums)-1
-        
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i]>=goal:
-                goal = i
-        return goal==0
+        reachable = 0
+        for i in range(len(nums)):
+            if i>reachable:
+                return False
+            reachable = max(reachable,i+nums[i])
+            
+        return True

3d5374872f829b13ed23ba6f0c61197c415458d7
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-09 17:19:32 +0530

    Time: 132 ms (95.44%), Space: 15 MB (58.61%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -17,11 +17,13 @@ class Solution:
         
         jumps = 0
         left = right = 0
+        
         while right<len(nums)-1:
             farthest = 0
             
             for i in range(left, right+1):
                 farthest = max(farthest, i+nums[i])
+                
             left = right+1
             right = farthest
             jumps += 1

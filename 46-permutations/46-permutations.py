e6e4280f7b8a02641927ea36bac532bb1e98da18
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-11 23:17:08 +0530

    Time: 61 ms (51.38%), Space: 14.1 MB (57.78%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -4,21 +4,39 @@
     
 ####################################################################################################################
 
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+        
+#         def generate(ds,nums,freq,ans):
+#             if len(ds)==len(nums):
+#                 ans.append(ds[:])
+#                 return
+#             for i in range(len(nums)):
+#                 if freq[i]!=1:
+#                     ds.append(nums[i])
+#                     freq[i]=1
+#                     generate(ds,nums,freq,ans)
+#                     ds.pop()
+#                     freq[i]=0
+#         freq = [0]*len(nums)
+#         ans = []
+#         generate([],nums,freq,ans)
+#         return ans
+
+#####################################################################################################################
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def generate(ds,nums,freq,ans):
-            if len(ds)==len(nums):
-                ans.append(ds[:])
-                return
-            for i in range(len(nums)):
-                if freq[i]!=1:
-                    ds.append(nums[i])
-                    freq[i]=1
-                    generate(ds,nums,freq,ans)
-                    ds.pop()
-                    freq[i]=0
-        freq = [0]*len(nums)
+        def generate(curr,nums,ans):
+            
+            if curr==len(nums): ans.append(nums[:]); return
+            
+            for i in range(curr,len(nums)):
+                nums[curr],nums[i] = nums[i],nums[curr]
+                generate(curr+1,nums,ans)
+                nums[curr],nums[i] = nums[i],nums[curr]
+                
         ans = []
-        generate([],nums,freq,ans)
+        generate(0,nums,ans)
         return ans

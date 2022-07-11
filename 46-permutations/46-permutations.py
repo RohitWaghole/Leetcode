a83431324545055d063c6dd1386031dc94bb3cf1
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-11 23:13:31 +0530

    Time: 64 ms (44.67%), Space: 13.9 MB (83.91%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,4 +1,24 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         return list(itertools.permutations(nums))
+    
+####################################################################################################################
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums))
-    
+        
+        def generate(ds,nums,freq,ans):
+            if len(ds)==len(nums):
+                ans.append(ds[:])
+                return
+            for i in range(len(nums)):
+                if freq[i]!=1:
+                    ds.append(nums[i])
+                    freq[i]=1
+                    generate(ds,nums,freq,ans)
+                    ds.pop()
+                    freq[i]=0
+        freq = [0]*len(nums)
+        ans = []
+        generate([],nums,freq,ans)
+        return ans

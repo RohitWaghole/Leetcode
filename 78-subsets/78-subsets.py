8d3e5c54f8071b80ab597827d54cabd6d951ff5c
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-11 22:36:52 +0530

    Time: 49 ms (59.17%), Space: 14 MB (81.60%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,86 +1,31 @@
 # class Solution:
 #     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-# #         1. Using Recursion
-# #         def sub(index,nums,ds,ans):
-# #             if index >= len(nums):
-# #                 ans.append(ds[:])
-# #                 return
-            
-# #             ds.append(nums[index])
-# #             sub(index+1,nums,ds,ans)
-# #             ds.pop()
-# #             sub(index+1,nums,ds,ans)
-        
-# #         ans=[]
-# #         sub(0,nums,[],ans)
-# #         return ans
-
-#         # 2. simple Method
-    
-#         ans = [[]]
-        
-#         for i in nums:
-#             ans += [j + [i] for j in ans]
-            
-#         return ans
-
-
-
-# class Solution:
-#     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-# #         1. Using Recursion
-
-#         def generate(i,ds,nums,ans):
-            
-#             if i>=len(nums):
+#         1. Using Recursion
+#         def sub(index,nums,ds,ans):
+#             if index >= len(nums):
 #                 ans.append(ds[:])
 #                 return
-#             ds.append(nums[i])
-#             generate(i+1,ds,nums,ans)
-#             ds.pop()
-#             generate(i+1,ds,nums,ans)
             
-#         ans = []
-#         generate(0,[],nums,ans)
+#             ds.append(nums[index])
+#             sub(index+1,nums,ds,ans)
+#             ds.pop()
+#             sub(index+1,nums,ds,ans)
+        
+#         ans=[]
+#         sub(0,nums,[],ans)
 #         return ans
 
+##############################################################################################################
+
+# 2. simple Method
+    
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-#         2. Using iterative solution
-        
         ans = [[]]
+        
         for i in nums:
-            ans += [curr + [i] for curr in ans]
+            ans += [j + [i] for j in ans]
+            
         return ans
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

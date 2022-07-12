bd6f75cc2a827ba8df818054473444d4f59d8c02
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-12 09:53:53 +0530

    Time: 36 ms (82.94%), Space: 13.9 MB (13.49%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
@@ -1,9 +1,21 @@
+# class Solution:
+#     def countNumbersWithUniqueDigits(self, n: int) -> int:
+        
+#         ans,curr=1,1
+#         choices = [9,9,8,7,6,5,4,3,2,1]
+#         for i in range(n):
+#             curr *= choices[i]
+#             ans += curr
+#         return ans
+
+###################################################################################################################
+
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
-        
-        ans,curr=1,1
-        choices = [9,9,8,7,6,5,4,3,2,1]
-        for i in range(n):
-            curr *= choices[i]
-            ans += curr
-        return ans
+        if n==0:
+            return 1
+        total,start = 10,9
+        for i in range(1,n):
+            start *= (10-i)
+            total += start
+        return total

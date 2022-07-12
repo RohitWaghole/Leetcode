7bf790325775944da741ef02eae1514d3c8f9439
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-12 09:35:49 +0530

    Time: 48 ms (71.44%), Space: 13.8 MB (96.55%) - LeetHub

--- 507-perfect-number/507-perfect-number.py
+++ 507-perfect-number/507-perfect-number.py
@@ -34,22 +34,24 @@
 
 ####################################################################################################################
 
+# class Solution:
+#     def checkPerfectNumber(self, n: int) -> bool:
+        
+#         s = 0
+#         for i in range(1,n):
+#             if i*i>n:
+#                 break
+#             if n%i==0:
+#                 s+=i
+#                 if i*i != n:
+#                     s += n//i
+#         return s-n==n
+        
+####################################################################################################################
+
 class Solution:
     def checkPerfectNumber(self, n: int) -> bool:
-        
-        s = 0
-        for i in range(1,n):
-            if i*i>n:
-                break
-            if n%i==0:
-                s+=i
-                if i*i != n:
-                    s += n//i
-        return s-n==n
-        
-        
-        
-        
+        return n in (6,28,496,8128,33550336)
         
         
         

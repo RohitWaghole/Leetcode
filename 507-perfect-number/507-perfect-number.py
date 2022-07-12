03f06cd34bc348de5fe28492e30ad57045e0eaf5
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-12 09:34:13 +0530

    Time: 90 ms (8.84%), Space: 13.8 MB (96.55%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.py
@@ -0,0 +1,56 @@
+# class Solution:
+#     def checkPerfectNumber(self, num: int) -> bool:
+        
+#         s = 0
+#         for i in range(1,num):
+#             if num%i==0:
+#                 s+=i
+#         return s==num
+    
+#####################################################################################################################
+
+# class Solution:
+#     def checkPerfectNumber(self, num: int) -> bool:
+#         s = 0
+#         for i in range(1,num):
+#             if num%i==0:
+#                 s+=i
+#             if s>num:
+#                 return False
+#         return s==num
+
+#####################################################################################################################
+
+# class Solution:
+#     def checkPerfectNumber(self, n: int) -> bool:
+        
+#         s = 0
+#         for i in range(1,n//2+1):
+#             if n%i==0:
+#                 s+=i
+#             if s>n:
+#                 return False
+#         return s==n
+
+####################################################################################################################
+
+class Solution:
+    def checkPerfectNumber(self, n: int) -> bool:
+        
+        s = 0
+        for i in range(1,n):
+            if i*i>n:
+                break
+            if n%i==0:
+                s+=i
+                if i*i != n:
+                    s += n//i
+        return s-n==n
+        
+        
+        
+        
+        
+        
+        
+        

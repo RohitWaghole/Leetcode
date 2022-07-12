602431de0e4cc77ca24a6ef1a4c5fe7f096ab6f7
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-12 09:46:36 +0530

    Time: 60 ms (21.03%), Space: 13.8 MB (56.15%) - LeetHub

---
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        return [1,
+                10,
+                91,
+                739,
+                5275,
+                32491,
+                168571,
+                712891,
+                2345851][n]

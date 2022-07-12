46a674fb3531df61a92f823df6147c86355893cc
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-12 14:19:06 +0530

    Time: 151 ms (46.43%), Space: 14.2 MB (68.32%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,7 +1,33 @@
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+#         nums = sorted(nums1+nums2)
+#         if len(nums)%2==0:
+#             return ((nums[len(nums)//2]+nums[len(nums)//2 -1])/2)
+#         return nums[len(nums)//2]
+
+###############################################################################################################
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
-        nums = sorted(nums1+nums2)
-        if len(nums)%2==0:
-            return ((nums[len(nums)//2]+nums[len(nums)//2 -1])/2)
-        return nums[len(nums)//2]
+        n = []
+        
+        i,j=0,0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                n.append(nums1[i])
+                i+=1
+            else:
+                n.append(nums2[j])
+                j+=1
+        while i<len(nums1):
+            n.append(nums1[i])
+            i+=1
+        while j<len(nums2):
+            n.append(nums2[j])
+            j+=1
+        
+        if len(n)%2==0:
+            return (n[len(n)//2]+n[len(n)//2-1])/2
+        return n[len(n)//2]

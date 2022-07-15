069e4527ad9a168e6a895d91b3508b43d3cb6ff4
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-15 20:06:17 +0530

    Time: 45 ms (52.84%), Space: 14 MB (11.65%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -12,13 +12,26 @@
 
 ##################################################################################################################
 
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n<=2:
+#             return n
+#         dp = [0]*(n)
+#         dp[0]=1
+#         dp[1]=2
+#         for i in range(2,n):
+#             dp[i]=dp[i-1]+dp[i-2]
+#         return dp[n-1]
+
+####################################################################################################################
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n<=2:
             return n
-        dp = [0]*(n)
-        dp[0]=1
-        dp[1]=2
-        for i in range(2,n):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n-1]
+        prev,prev2=1,2
+        for i in range(3,n+1):
+            nxt = prev+prev2
+            prev=prev2
+            prev2 = nxt
+        return nxt

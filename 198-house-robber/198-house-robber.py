94c1b8f85162604a28369e909ded04e70c559239
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-19 19:08:56 +0530

    Time: 47 ms (55.42%), Space: 13.9 MB (65.75%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,41 +1,42 @@
+'''
+Recursion -> TLE
+T.C. => O(N)
+S.C. => O(N)(RECURSION STACK SPACE)
+'''
+
 # class Solution:
 #     def rob(self, nums: List[int]) -> int:
-        
 #         def f(i,nums):
-            
 #             if i==0:
 #                 return nums[i]
 #             if i<0:
 #                 return 0
-            
-            
 #             take = nums[i] + f(i-2,nums)
 #             notTake = 0 + f(i-1,nums)
-            
 #             return max(take,notTake)
-        
 #         return f(len(nums)-1,nums)
 
 
 ##########################################################################################################
 
+'''
+RECURSION + DP : MEMOIZATION -> ACCEPTED
+T.C. -> O(N)
+S.C. -> O(N)(ARRAY) + O(N)(RECURSION STACK SPACE)
+'''
 
 # class Solution:
 #     def rob(self, nums: List[int]) -> int:
         
 #         def f(i,nums,dp):
-            
 #             if i==0:
 #                 return nums[i]
 #             if i<0:
 #                 return 0
-            
 #             if dp[i]!=-1:
 #                 return dp[i]
-            
 #             take = nums[i] + f(i-2,nums,dp)
 #             notTake = 0 + f(i-1,nums,dp)
-            
 #             dp[i] = max(take,notTake)
 #             return dp[i]
         
@@ -45,17 +46,45 @@
 
 ##########################################################################################################
 
+'''
+DP + TABULATION -> ACCEPTED
+T.C. -> O(N)
+S.C. -> O(N)(ARRAY)
+'''
+
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+        
+#         dp = [-1]*(len(nums))
+#         dp[0] = nums[0]
+#         for i in range(1,len(nums)):
+#             take = nums[i]
+#             if i>1:
+#                 take += dp[i-2]
+#             notTake = 0 + dp[i-1]
+#             dp[i] = max(take,notTake)
+#         return dp[-1]
+
+
+##########################################################################################################
+
+'''
+TABULATION WITH SPACE OPTIMIZED -> ACCEPTED
+T.C. -> O(N)
+S.C. -> O(1)
+'''
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        dp = [-1]*(len(nums))
-        dp[0] = nums[0]
+        prev2 = 0
+        prev = nums[0]
         for i in range(1,len(nums)):
             take = nums[i]
             if i>1:
-                take += dp[i-2]
-            notTake = 0 + dp[i-1]
-            
-            dp[i] = max(take,notTake)
-        return dp[-1]
+                take += prev2
+            notTake = prev
+            curi = max(take,notTake)
+            prev2 = prev
+            prev = curi
+        return prev

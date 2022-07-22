1370acce00ffff68c5d774334a0f66171353e4d6
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-22 15:19:35 +0530

    Time: 31 ms (94.44%), Space: 13.9 MB (32.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -40,23 +40,42 @@
 
 #####################################################################################################
 
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+        
+#         dp = [[0]*n for i in range(m)]
+        
+#         dp[0][0] = 1
+#         for i in range(m):
+#             for j in range(n):
+#                 if i==0 and j==0:
+#                     dp[i][j] = 1
+#                     continue
+#                 up,left = 0,0
+#                 if i>0: up = dp[i-1][j]
+#                 if j>0: left = dp[i][j-1]
+                
+#                 dp[i][j] = up + left
+#         return dp[m-1][n-1]
+        
+
+#####################################################################################################
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [[0]*n for i in range(m)]
-        
-        dp[0][0] = 1
+        dp = [0]*n
+        dp[0] = 1
         for i in range(m):
+            temp = [0]*n
             for j in range(n):
-                if i==0 and j==0:
-                    dp[i][j] = 1
-                    continue
-                down,right = 0,0
-                if i>0: down = dp[i-1][j]
-                if j>0: right = dp[i][j-1]
+                up,left = 0,0
+                up = dp[j]
+                if j>0: left = temp[j-1]
                 
-                dp[i][j] = down + right
-        return dp[m-1][n-1]
+                temp[j] = up + left
+            dp = temp
+        return dp[-1]
         
         
         

35759c1e9862b5c666b95cbcce395681f6f9f066
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-23 14:48:50 +0530

    Time: 69 ms (94.69%), Space: 15.4 MB (22.31%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -15,20 +15,43 @@
 
 ###########################################################################################
 
+# class Solution:
+#     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+#         def f(i,j,triangle,n,dp):
+            
+#             if i==n-1:
+#                 return triangle[n-1][j]
+#             if dp[i][j]!=-1:
+#                 return dp[i][j]
+#             down = f(i+1,j,triangle,n,dp)
+#             diagonal = f(i+1,j+1,triangle,n,dp)
+#             dp[i][j] = min(down,diagonal) + triangle[i][j]
+#             return dp[i][j]
+        
+#         n = len(triangle[-1])
+#         dp = [[-1]*n for i in range(n)]
+#         return f(0,0,triangle,n,dp)
+
+###########################################################################################
+
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
-        def f(i,j,triangle,n,dp):
+        def f(triangle,n,dp):
             
-            if i==n-1:
-                return triangle[n-1][j]
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            down = f(i+1,j,triangle,n,dp)
-            diagonal = f(i+1,j+1,triangle,n,dp)
-            dp[i][j] = min(down,diagonal) + triangle[i][j]
-            return dp[i][j]
+            for j in range(n):
+                dp[n-1][j] = triangle[n-1][j]
+                
+            for i in range(n-2,-1,-1):
+                for j in range(i,-1,-1):
+                    down = dp[i+1][j]
+                    diagonal = dp[i+1][j+1]
+                    dp[i][j] = min(down,diagonal) + triangle[i][j]
+            return dp[0][0]
         
         n = len(triangle[-1])
         dp = [[-1]*n for i in range(n)]
-        return f(0,0,triangle,n,dp)
+        return f(triangle,n,dp)
+    
+    

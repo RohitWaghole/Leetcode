887f9664b9e720a2369e0410db695a28c64b3c8c
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-24 10:38:34 +0530

    Time: 111 ms (5.26%), Space: 14.4 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,10 +1,20 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]==target:
-                    return True
+        for i in matrix:
+            if target in i:
+                return True
         return False
+
+    
+####################################################################################
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]==target:
+#                     return True
+#         return False
 
 ####################################################################################
 

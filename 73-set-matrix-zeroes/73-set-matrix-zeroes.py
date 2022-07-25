54409f3e6a44b5d3e355eceefb219f5217ee81ef
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 10:05:09 +0530

    Time: 213 ms (41.68%), Space: 14.7 MB (90.07%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,17 +1,34 @@
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+        
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]==0:
+
+#                     row = i
+#                     col = j
+#                     for m in range(len(matrix[0])):
+#                         if matrix[row][m]!=0: matrix[row][m] = 'a'
+#                     for m in range(len(matrix)):
+#                         if matrix[m][col]!=0: matrix[m][col] = 'a'
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]=='a':
+#                     matrix[i][j] = 0
+
+##########################################################################################################
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         
+        r = ['a']*len(matrix)
+        c = ['a']*len(matrix[0])
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]==0:
-
-                    row = i
-                    col = j
-                    for m in range(len(matrix[0])):
-                        if matrix[row][m]!=0: matrix[row][m] = 'a'
-                    for m in range(len(matrix)):
-                        if matrix[m][col]!=0: matrix[m][col] = 'a'
+                    r[i]=0
+                    c[j]=0
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j]=='a':
+                if r[i]==0 or c[j]==0:
                     matrix[i][j] = 0

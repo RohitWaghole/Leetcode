27eebbcdfa7821189a95e01c3d7a9a1f49e68416
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 11:35:26 +0530

    Time: 48 ms (49.28%), Space: 13.9 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,11 +1,11 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def generate(self, n: int) -> List[List[int]]:
         
-        pascalTriangle = [[1],[1,1]]
-        for i in range(numRows-2):
-            l = [1]
-            for m in range(len(pascalTriangle[-1])-1):
-                l.append(pascalTriangle[-1][m]+pascalTriangle[-1][m+1])
-            l+=[1]
-            pascalTriangle.append(l)
-        return pascalTriangle[:numRows]
+        pascal = [[1]]
+        for i in range(n-1):
+            temp = [1]
+            for j in range(len(pascal[-1])-1):
+                temp.append(pascal[-1][j]+pascal[-1][j+1])
+            temp.append(1)
+            pascal.append(temp)
+        return pascal

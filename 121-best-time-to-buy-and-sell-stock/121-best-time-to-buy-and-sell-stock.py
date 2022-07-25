1c667ebcd8fea402fc2803e41c35e9b7aaabf84d
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 23:16:49 +0530

    Time: 1742 ms (34.76%), Space: 25 MB (37.97%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,21 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         buy,sell = float('inf'),0
+#         for stock in prices:
+#             buy = min(buy, stock)
+#             sell = max(sell, stock - buy)
+#         return sell
+
+####################################################################################
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        buy,sell = float('inf'),0
+        buy, sell = prices[0],0
         for stock in prices:
-            buy = min(buy, stock)
-            sell = max(sell, stock - buy)
+            if stock<buy:
+                buy = stock
+            if stock - buy > sell:
+                sell = stock - buy
         return sell

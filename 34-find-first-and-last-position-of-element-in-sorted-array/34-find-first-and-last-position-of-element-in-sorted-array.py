363e4ca95e5863d5dc670bc4bf8782c7c839fa0e
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 08:41:52 +0530

    Time: 101 ms (77.14%), Space: 15.4 MB (50.23%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,11 +1,41 @@
+# class Solution:
+#     def searchRange(self, nums: List[int], target: int) -> List[int]:
+#         first = -1
+#         last = -1
+#         for i in range(len(nums)):
+#             if nums[i] == target and first==-1:
+#                 first = i
+#                 last = i
+#             elif nums[i]==target:
+#                 last = i
+#         return [first,last]
+
+####################################################################################################
+
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = -1
-        last = -1
-        for i in range(len(nums)):
-            if nums[i] == target and first==-1:
-                first = i
-                last = i
-            elif nums[i]==target:
-                last = i
-        return [first,last]
+
+        l = self.searchLeft(nums,target)
+        r = self.searchRight(nums,target)
+        
+        return [l,r] if l<=r else [-1,-1]
+    
+    def searchLeft(self,a,x):
+        lo,hi = 0,len(a)-1
+        while lo<=hi:
+            m = (lo+hi)//2
+            if x>a[m]:
+                lo=m+1
+            else:
+                hi=m-1
+        return lo
+    
+    def searchRight(self,a,x):
+        lo,hi = 0,len(a)-1
+        while lo<=hi:
+            m = (lo+hi)//2
+            if x>=a[m]:
+                lo=m+1
+            else:
+                hi=m-1
+        return hi

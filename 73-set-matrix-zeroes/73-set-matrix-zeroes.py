de358ff187a160ca3cd589aa1a3a69e16aba8a9a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-25 10:22:53 +0530

    Time: 231 ms (30.08%), Space: 14.8 MB (15.43%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -18,17 +18,35 @@
 
 ##########################################################################################################
 
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+        
+#         r = ['a']*len(matrix)
+#         c = ['a']*len(matrix[0])
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]==0:
+#                     r[i]=0
+#                     c[j]=0
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if r[i]==0 or c[j]==0:
+#                     matrix[i][j] = 0
+
+##########################################################################################################
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        
-        r = ['a']*len(matrix)
-        c = ['a']*len(matrix[0])
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        col0 = 1
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in range(row):
+            if matrix[i][0]==0: col0=0
+            for j in range(1,col):
                 if matrix[i][j]==0:
-                    r[i]=0
-                    c[j]=0
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if r[i]==0 or c[j]==0:
+                    matrix[i][0] = matrix[0][j] = 0
+        for i in range(row-1,-1,-1):
+            for j in range(col-1,0,-1):
+                if matrix[i][0]==0 or matrix[0][j]==0:
                     matrix[i][j] = 0
+            if col0==0: matrix[i][0] = 0

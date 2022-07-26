557a0952259fa0b623a9ac50b45eac9c52a5900f
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-26 14:58:16 +0530

    Time: 173 ms (7.85%), Space: 18.7 MB (68.08%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,29 +5,45 @@
 #         self.left = None
 #         self.right = None
 
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+#         def f(root,target,ds):
+#             if not root:
+#                 return False
+#             ds.append(root.val)
+#             if root==target:
+#                 return True
+#             left = f(root.left,target,ds)
+#             right = f(root.right,target,ds)
+            
+#             if left or right:
+#                 return True
+#             ds.pop()
+#             return False
+        
+#         l1,l2 = [],[]
+#         f(root,p,l1)
+#         f(root,q,l2)
+        
+#         i,j = 0,0
+#         while i<len(l1) and j<len(l2) and l1[i]==l2[j]:
+#             i+=1
+#             j+=1
+#         return TreeNode(l1[i-1])
+
+#############################################################################################################
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        def f(root,target,ds):
-            if not root:
-                return False
-            ds.append(root.val)
-            if root==target:
-                return True
-            left = f(root.left,target,ds)
-            right = f(root.right,target,ds)
-            
-            if left or right:
-                return True
-            ds.pop()
-            return False
+        if root==None or root==p or root==q:
+            return root
         
-        l1,l2 = [],[]
-        f(root,p,l1)
-        f(root,q,l2)
-        
-        i,j = 0,0
-        while i<len(l1) and j<len(l2) and l1[i]==l2[j]:
-            i+=1
-            j+=1
-        return TreeNode(l1[i-1])
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if left==None:
+            return right
+        if right==None:
+            return left
+        return root

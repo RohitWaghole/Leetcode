fff98179e3b909828539c9ade21c130e1cc77d57
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-26 19:15:30 +0530

    Time: 83 ms (5.41%), Space: 14 MB (30.03%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -27,7 +27,7 @@ class Solution:
         n = len(matrix)
         
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(0,i):
                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
         
         def rev(a):

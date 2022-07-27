a6cc77f3709c4309bf294cda1bfefa537e9dfa9a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-27 11:20:27 +0530

    Time: 68 ms (28.95%), Space: 15.3 MB (10.29%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -4,15 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def __init__(self):
-        self.prev = None
-    def flatten(self, root: Optional[TreeNode]) -> None:
-        if root==None:
-            return 
-        self.flatten(root.right)
-        self.flatten(root.left)
+
+# T.C. -> O(N)
+# S.C. -> O(N)
+# class Solution:
+#     def __init__(self):
+#         self.prev = None
+#     def flatten(self, root: Optional[TreeNode]) -> None:
+#         if root==None:
+#             return 
+#         self.flatten(root.right)
+#         self.flatten(root.left)
         
-        root.right = self.prev
-        root.left = None
-        self.prev = root
+#         root.right = self.prev
+#         root.left = None
+#         self.prev = root
+
+#######################################################################################
+
+# T.C. -> O(N)
+# S.C. -> O(N)
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if not root:
+            return
+        stack = [root]
+        while stack:
+            curr = stack.pop()
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+            if stack:
+                curr.right = stack[-1]
+            curr.left = None

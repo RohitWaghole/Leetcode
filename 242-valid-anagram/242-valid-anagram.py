6128f0fe75d3f0a70b4221b9764c240deba7fee6
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-28 09:57:48 +0530

    Time: 83 ms (41.49%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -12,29 +12,29 @@
 
 ##########################################################################################
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        ls,lt = [0]*26,[0]*26
-        if len(s)!=len(t):
-            return False
-        for i in range(len(s)):
-            ls[ord(s[i])-ord('a')]+=1
-            lt[ord(t[i])-ord('a')]+=1
-        for i in range(26):
-            if ls[i]!=lt[i]:
-                return False
-        return True
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         ls,lt = [0]*26,[0]*26
+#         if len(s)!=len(t):
+#             return False
+#         for i in range(len(s)):
+#             ls[ord(s[i])-ord('a')]+=1
+#             lt[ord(t[i])-ord('a')]+=1
+#         for i in range(26):
+#             if ls[i]!=lt[i]:
+#                 return False
+#         return True
 
 ##########################################################################################
 
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s)!=len(t): return False
-#         l = [0]*256
-#         for i in range(len(s)):
-#             l[ord(s[i])]+=1
-#             l[ord(t[i])]-=1
-#         for i in l:
-#             if i!=0:
-#                 return False
-#         return True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t): return False
+        l = [0]*256
+        for i in range(len(s)):
+            l[ord(s[i])]+=1
+            l[ord(t[i])]-=1
+        for i in l:
+            if i!=0:
+                return False
+        return True

ef8fa8bce083fed3e11f1e9c8ef34857c6a9892a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-28 09:56:14 +0530

    Time: 85 ms (38.55%), Space: 14.5 MB (66.69%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,40 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s)==sorted(t)
+
+#########################################################################################
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         sd = Counter(s)
+#         td = Counter(t)
+#         return sd==td
+
+##########################################################################################
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         ls,lt = [0]*26,[0]*26
+#         if len(s)!=len(t):
+#             return False
+#         for i in range(len(s)):
+#             ls[ord(s[i])-ord('a')]+=1
+#             lt[ord(t[i])-ord('a')]+=1
+#         for i in range(26):
+#             if ls[i]!=lt[i]:
+#                 return False
+#         return True
+
+##########################################################################################
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t): return False
+        l = [0]*256
+        for i in range(len(s)):
+            l[ord(s[i])]+=1
+            l[ord(t[i])]-=1
+        for i in l:
+            if i!=0:
+                return False
+        return True

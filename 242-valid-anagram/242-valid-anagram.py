2f7a0b6b5205c788f910df3d171cedb5963c7e0a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-28 09:56:49 +0530

    Time: 102 ms (17.86%), Space: 15.2 MB (11.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,6 @@
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         return sorted(s)==sorted(t)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
 
 #########################################################################################
 
@@ -27,14 +27,14 @@
 
 ##########################################################################################
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t): return False
-        l = [0]*256
-        for i in range(len(s)):
-            l[ord(s[i])]+=1
-            l[ord(t[i])]-=1
-        for i in l:
-            if i!=0:
-                return False
-        return True
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s)!=len(t): return False
+#         l = [0]*256
+#         for i in range(len(s)):
+#             l[ord(s[i])]+=1
+#             l[ord(t[i])]-=1
+#         for i in l:
+#             if i!=0:
+#                 return False
+#         return True

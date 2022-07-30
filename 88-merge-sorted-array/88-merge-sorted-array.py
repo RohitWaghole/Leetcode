c88b73c81e552b59042c2d2403caf6c7d3912c19
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-07-30 21:56:26 +0530

    Time: 58 ms (51.37%), Space: 13.9 MB (37.97%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,11 +1,20 @@
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+#         l = []
+#         for i in range(m):
+#             l.append(nums1[i])
+#         for i in range(n):
+#             l.append(nums2[i])
+#         l.sort()
+#         for i in range(m+n):
+#             nums1[i] = l[i]
+
+###############################################################################################
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         
-        l = []
-        for i in range(m):
-            l.append(nums1[i])
         for i in range(n):
-            l.append(nums2[i])
-        l.sort()
-        for i in range(m+n):
-            nums1[i] = l[i]
+            nums1[m+i] = nums2[i]
+        nums1.sort()

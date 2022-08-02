2e768d07b0a471e7572dee1a35e56f3d84ac17ae
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-02 10:12:03 +0530

    Time: 279 ms (23.30%), Space: 18.1 MB (85.07%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -4,26 +4,60 @@ Time Complexity: O(NlogN)+O(N*N). O(NlogN) for sorting the array, and O(N*N) bec
 Space Complexity: O(N), as we are using a separate data structure.
 '''
 
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+#         intervals.sort()
+#         ans = []
+        
+#         for i in range(len(intervals)):
+            
+#             start,end = intervals[i]
+            
+#             if ans!=[]:
+                
+#                 if start<= ans[-1][1]:
+#                     continue
+            
+#             for j in range(i+1, len(intervals)):
+#                 if intervals[j][0] <= end:
+#                     end = max(end, intervals[j][1])
+            
+#             end = max(end, intervals[i][1])
+#             ans.append([start, end])
+            
+#         return ans
+    
+#########################################################################################################
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        intervals.sort()
-        ans = []
         
-        for i in range(len(intervals)):
+        intervals.sort()
+        res = []
+        a = intervals[0]
+        
+        for i in range(1,len(intervals)):
+            f = 0
+            b = intervals[i]
             
-            start,end = intervals[i]
-            
-            if ans!=[]:
-                
-                if start<= ans[-1][1]:
-                    continue
-            
-            for j in range(i+1, len(intervals)):
-                if intervals[j][0] <= end:
-                    end = max(end, intervals[j][1])
-            
-            end = max(end, intervals[i][1])
-            ans.append([start, end])
-            
-        return ans
+            if a[1]>=b[0]:
+                a[1] = max(a[1],b[1])
+            else:
+                res.append(a)
+                f = 1
+                a = intervals[i]
+        res.append(a)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

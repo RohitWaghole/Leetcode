a812706c85710c934736f0845e26dca3ff039109
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-02 09:07:32 +0530

    Time: 265 ms (67.82%), Space: 18.8 MB (17.04%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,19 +1,28 @@
-class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        l = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                l.append(matrix[i][j])
-        l.sort()
-        return l[k-1]
+# class Solution:
+#     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+#         l = []
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 l.append(matrix[i][j])
+#         l.sort()
+#         return l[k-1]
 
 ##############################################################################################
 
-# class Solution:
-#     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-#         count = 0
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix[0])):
-#                 count+=1
-#                 if count==k:
-#                     return matrix[i][j]
+'''
+The property of HEAP data structure in Python is that each time the smallest of heap element is popped(min heap). Whenever elements are pushed or popped, heap structure in maintained. The heap[0] element also returns the smallest element each time.
+'''
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        
+        maxHeap = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                
+                heappush(maxHeap,-matrix[i][j]) # adds elements in the list
+                
+                if len(maxHeap)>k:
+                    heappop(maxHeap) # pops the smallest element from the list regardless of its position
+                    
+        return -heappop(maxHeap)

bc3c5388688c0418fb5a52154e864ca0fede6a1b
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-11 14:41:40 +0530

    Time: 63 ms (86.42%), Space: 18 MB (91.08%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -4,21 +4,72 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# class Solution:
+#     def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        
+#         self.d = {}
+        
+#         def traverse(root):
+            
+#             if root:
+#                 traverse(root.left)
+#                 self.d[root.val] = self.d.get(root.val, 0)+1
+#                 traverse(root.right)
+                
+#         traverse(root)
+#         res = []
+#         mx = max(self.d.values())
+#         for key,val in self.d.items():
+#             if val==mx: res.append(key)
+#         return res
+
+###########################################################################################################
+
+
 class Solution:
+    
+    prev = None
+    current_count = 0
+    max_count = 0
+    result = []
+    
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         
-        self.d = {}
-        
-        def traverse(root):
+        self.dfs(root)
+        return self.result
+    
+    def dfs(self, root):
+        if root:
             
-            if root:
-                traverse(root.left)
-                self.d[root.val] = self.d.get(root.val, 0)+1
-                traverse(root.right)
+            self.dfs(root.left)
+            
+            if root.val==self.prev:
+                self.current_count += 1
+            else:
+                self.current_count = 1
                 
-        traverse(root)
-        res = []
-        mx = max(self.d.values())
-        for key,val in self.d.items():
-            if val==mx: res.append(key)
-        return res
+            if self.current_count==self.max_count:
+                self.result.append(root.val)
+                
+            elif self.current_count>self.max_count:
+                self.result = [root.val]
+                self.max_count = self.current_count
+            self.prev = root.val
+            
+            self.dfs(root.right)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

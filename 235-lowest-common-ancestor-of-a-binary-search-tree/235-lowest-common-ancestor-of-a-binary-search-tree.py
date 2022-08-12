4ff19391e6dfe54f49427c52461346081d11eb14
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-12 19:26:50 +0530

    Time: 190 ms (5.10%), Space: 18.8 MB (22.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -50,12 +50,84 @@
 
 #############################################################################################################
 
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         while root:
+#             if p.val < root.val > q.val:
+#                 root = root.left
+#             elif p.val > root.val < q.val:
+#                 root = root.right
+#             else:
+#                 return root
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############################################################################################################
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            if p.val < root.val > q.val:
-                root = root.left
-            elif p.val > root.val < q.val:
-                root = root.right
-            else:
-                return root
+        
+        def f(root, target, ds):
+            
+            if root:
+                
+                ds.append(root.val)
+                if root==target:
+                    return True
+                
+                left = f(root.left, target, ds)
+                right = f(root.right, target, ds)
+                
+                if left or right:
+                    return True
+                ds.pop()
+                
+            return False
+        
+        l1,l2 = [],[]
+        f(root, p, l1)
+        f(root, q, l2)
+        
+        i,j=0,0
+        while i<len(l1) and j<len(l2) and l1[i]==l2[j]:
+            i+=1
+            j+=1
+        return TreeNode(l1[i-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

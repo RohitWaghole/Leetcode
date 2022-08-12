96e0f3042e1cb01d7349ef4dc253bd0737630541
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-12 19:45:20 +0530

    Time: 118 ms (56.45%), Space: 18.8 MB (22.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -50,66 +50,14 @@
 
 #############################################################################################################
 
-# class Solution:
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         while root:
-#             if p.val < root.val > q.val:
-#                 root = root.left
-#             elif p.val > root.val < q.val:
-#                 root = root.right
-#             else:
-#                 return root
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#############################################################################################################
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        if root==None or root==p or root==q:
-            return root
-        
-        left = self.lowestCommonAncestor(root.left,p,q)
-        right = self.lowestCommonAncestor(root.right,p,q)
-        
-        if left==None:
-            return right
-        if right==None:
-            return left
-        return root
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
 
 

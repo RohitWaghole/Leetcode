0067a4c5d0bf75254652a48ef5bfd46e943fc933
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-12 20:40:30 +0530

    Time: 34 ms (90.64%), Space: 14.1 MB (28.88%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.py
+++ 1496-path-crossing/1496-path-crossing.py
@@ -1,20 +1,42 @@
+# class Solution:
+#     def isPathCrossing(self, path: str) -> bool:
+#         res = {'0,0':0}
+#         c = 1
+#         x,y = 0,0
+#         for i in path:
+#             if i=='N':
+#                 y+=1
+#             elif i=='E':
+#                 x+=1
+#             elif i=='S':
+#                 y-=1
+#             else:
+#                 x-=1
+#             t = str(x)+','+str(y)
+#             if t in res:
+#                 return True
+#             res[t]=c
+#             c+=1
+#         return False
+
+###########################################################################################
+
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        res = {'0,0':0}
-        c = 1
+        
         x,y = 0,0
+        s = {(x,y)}
+        
         for i in path:
-            if i=='N':
+            if i=="N":
                 y+=1
-            elif i=='E':
-                x+=1
-            elif i=='S':
+            elif i=="S":
                 y-=1
+            elif i=="E":
+                x+=1
             else:
                 x-=1
-            t = str(x)+','+str(y)
-            if t in res:
+            if (x,y) in s:
                 return True
-            res[t]=c
-            c+=1
+            s.add((x,y))
         return False

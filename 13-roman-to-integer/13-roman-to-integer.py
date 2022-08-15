d960dc2e4da09294e20ea2c4465e04b63f9c942e
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-15 19:23:58 +0530

    Time: 31 ms (99.95%), Space: 14 MB (31.19%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,20 +1,35 @@
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+        
+#         d = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5, 'IX':9, 'X':10, 'XL':40, 'L':50, 'XC':90, 'C':100, 'CD':400, 'D':500, 'CM':900, 'M':1000}
+        
+#         s = s[::-1]
+#         ans = 0
+#         i = 0
+#         while i<len(s):
+            
+#             if s[i]=='I':
+#                 ans+=d[s[i]]
+#                 i+=1
+#             elif i+1<len(s) and s[i]!=s[i+1] and (s[i+1]+s[i]) in d:
+#                 ans += d[s[i+1]+s[i]]
+#                 i+=2
+#             else:
+#                 ans+=d[s[i]]
+#                 i+=1
+#         return ans
+
+###########################################################################################################
+
 class Solution:
     def romanToInt(self, s: str) -> int:
         
-        d = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5, 'IX':9, 'X':10, 'XL':40, 'L':50, 'XC':90, 'C':100, 'CD':400, 'D':500, 'CM':900, 'M':1000}
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         
-        s = s[::-1]
-        ans = 0
-        i = 0
-        while i<len(s):
-            
-            if s[i]=='I':
-                ans+=d[s[i]]
-                i+=1
-            elif i+1<len(s) and s[i]!=s[i+1] and (s[i+1]+s[i]) in d:
-                ans += d[s[i+1]+s[i]]
-                i+=2
+        z = 0
+        for i in range(len(s)-1):
+            if d[s[i]]<d[s[i+1]]:
+                z-=d[s[i]]
             else:
-                ans+=d[s[i]]
-                i+=1
-        return ans
+                z+=d[s[i]]
+        return z+d[s[-1]]

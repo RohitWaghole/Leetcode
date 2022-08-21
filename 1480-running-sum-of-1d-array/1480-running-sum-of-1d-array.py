8271c6fc704133bc721257465e34a05f39fe3771
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-21 12:34:48 +0530

    Time: 49 ms (80.06%), Space: 14.1 MB (73.04%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,9 +1,18 @@
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+        
+#         result = [None]*len(nums)
+#         s = 0
+#         for i in range(len(nums)):
+#             s+=nums[i]
+#             result[i]=s
+#         return result
+
+#############################################################################################
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         
-        result = [None]*len(nums)
-        s = 0
-        for i in range(len(nums)):
-            s+=nums[i]
-            result[i]=s
-        return result
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        return nums

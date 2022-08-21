c12b2ca3d268449d37665c9a67762a586fafb861
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-21 12:43:52 +0530

    Time: 634 ms (60.36%), Space: 26.1 MB (30.33%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,3 +1,12 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         return not len(nums)==len(set(nums))
+
+###############################################################################
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return not len(nums)==len(set(nums))
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]:
+                return True
+        return False

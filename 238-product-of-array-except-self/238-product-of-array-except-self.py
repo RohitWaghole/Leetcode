46fc5366bb0328875858883cd18894514f417f17
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-21 14:34:39 +0530

    Time: 309 ms (68.65%), Space: 22.2 MB (28.05%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -11,19 +11,41 @@
 
 ###############################################################################
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+#         prod = 1
+#         zero = nums.count(0)
+#         if zero>1: return [0]*len(nums)
+        
+#         for i in nums: prod *= i if i!=0 else 1
+        
+#         for i in range(len(nums)):
+#             if zero:
+#                 nums[i] = 0 if nums[i]!=0 else prod
+#             else:
+#                 nums[i] = prod//nums[i]
+        
+#         return nums
+
+###############################################################################
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        prod = 1
-        zero = nums.count(0)
-        if zero>1: return [0]*len(nums)
-        
-        for i in nums: prod *= i if i!=0 else 1
-        
+        pref, suff = [], []
+        s1,s2 = 1,1
         for i in range(len(nums)):
-            if zero:
-                nums[i] = 0 if nums[i]!=0 else prod
-            else:
-                nums[i] = prod//nums[i]
+            s1 *= nums[i]
+            s2 *= nums[len(nums)-i-1]
+            pref.append(s1)
+            suff.append(s2)
+            
+        suff = suff[::-1]
+        n = len(nums)
         
-        return nums
+        res = []
+        for i in range(n):
+            x = pref[i-1] if i else 1
+            y = suff[i+1] if i+1<n else 1
+            res.append(x*y)
+        return res

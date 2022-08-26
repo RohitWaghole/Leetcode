d7350dad71a05bb00cc098453fe39343025b7b93
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-26 09:08:04 +0530

    Time: 48 ms (73.80%), Space: 13.8 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,7 +1,19 @@
+# class Solution:
+#     def reorderedPowerOf2(self, n: int) -> bool:
+        
+#         for perm in itertools.permutations(str(n)):
+#             if perm[0] != '0' and bin(int(''.join(perm))).count('1')==1:
+#                 return True
+#         return False
+
+##################################################################################################
+
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         
-        for perm in itertools.permutations(str(n)):
-            if perm[0] != '0' and bin(int(''.join(perm))).count('1')==1:
+        d = collections.Counter(str(n))
+        for i in range(31):
+            s = collections.Counter(str(2**i))
+            if s==d:
                 return True
         return False

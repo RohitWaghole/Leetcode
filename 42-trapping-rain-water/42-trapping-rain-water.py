0889a017f5134b759ba5b5e58c9db4290a667b49
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-27 09:46:28 +0530

    Time: 232 ms (32.74%), Space: 16.1 MB (45.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,16 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pref, suff = [], []
-        mx = 0
-        for i in height:
-            mx = max(mx, i)
-            pref.append(mx)
-        mx = 0
-        for i in height[::-1]:
-            mx = max(mx,i)
-            suff.append(mx)
-        suff = suff[::-1]
+        
+        n = len(height)
         res = 0
-        for i in range(len(height)):
-            res += (min(pref[i],suff[i])-height[i])
+        max1, max2 = 0, 0
+        prefix = [0]*n
+        suffix = [0]*n
+        
+        for i in range(n):
+            max1 = max(max1, height[i])
+            max2 = max(max2, height[n-i-1])
+            prefix[i] = max1
+            suffix[n-i-1] = max2
+        
+        for i in range(n):
+            res += ( min(prefix[i], suffix[i]) - height[i] )
+        
         return res

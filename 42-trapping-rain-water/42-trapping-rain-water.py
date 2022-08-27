0d7515e76de00bf8b4bd161d1014354f72b3390f
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-27 12:02:45 +0530

    Time: 204 ms (49.67%), Space: 16 MB (81.74%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,19 +1,42 @@
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+        
+#         n = len(height)
+#         res = 0
+#         max1, max2 = 0, 0
+#         prefix = [0]*n
+#         suffix = [0]*n
+        
+#         for i in range(n):
+#             max1 = max(max1, height[i])
+#             max2 = max(max2, height[n-i-1])
+#             prefix[i] = max1
+#             suffix[n-i-1] = max2
+        
+#         for i in range(n):
+#             res += ( min(prefix[i], suffix[i]) - height[i] )
+        
+#         return res
+
+########################################################################################################
+
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-        n = len(height)
-        res = 0
-        max1, max2 = 0, 0
-        prefix = [0]*n
-        suffix = [0]*n
+        leftMax, rightMax = 0, 0
+        totalWater = 0
         
-        for i in range(n):
-            max1 = max(max1, height[i])
-            max2 = max(max2, height[n-i-1])
-            prefix[i] = max1
-            suffix[n-i-1] = max2
+        left, right = 0, len(height)-1
         
-        for i in range(n):
-            res += ( min(prefix[i], suffix[i]) - height[i] )
-        
-        return res
+        while left<=right:
+            
+            if leftMax <= rightMax:
+                leftMax = max(leftMax, height[left])
+                totalWater += leftMax - height[left]
+                left += 1
+            else:
+                rightMax = max(rightMax, height[right])
+                totalWater += rightMax - height[right]
+                right -= 1
+                
+        return totalWater

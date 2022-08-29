b267bb95a570bc66b4b5f1dc4025606ec357f60b
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-08-29 10:01:52 +0530

    Time: 127 ms (59.04%), Space: 14.9 MB (6.34%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,14 +1,35 @@
+# class Solution:
+#     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+
+#         d = defaultdict(list)
+#         m,n = len(mat), len(mat[0])
+#         for i in range(m):
+#             for j in range(n):
+#                 d[i-j].append(mat[i][j])
+        
+#         for key, val in d.items():
+#             s = sorted(val)
+#             d[key] = s
+            
+#         for i in range(m):
+#             for j in range(n):
+#                 diff = i-j
+#                 mat[i][j] = d[diff].pop(0)
+        
+#         return mat
+
+##################################################################################################
+
+from sortedcontainers import SortedList
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        d = defaultdict(list)
+        
+        
+        d = defaultdict(SortedList)
         m,n = len(mat), len(mat[0])
         for i in range(m):
             for j in range(n):
-                d[i-j].append(mat[i][j])
-        
-        for key, val in d.items():
-            s = sorted(val)
-            d[key] = s
+                d[i-j].add(mat[i][j])
             
         for i in range(m):
             for j in range(n):

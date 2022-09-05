03e88efbe9ce8b0c1b0e710834ff6af431ad6e66
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-05 10:57:50 +0530

    Time: 367 ms (5.01%), Space: 14.8 MB (16.59%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,125 +1,25 @@
-# # T.C. -> O(N x M + (N + M))
-# # S.C. -> O(1)
-
-# class Solution:
-#     def setZeroes(self, matrix: List[List[int]]) -> None:
-        
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix[0])):
-#                 if matrix[i][j]==0:
-
-#                     row = i
-#                     col = j
-#                     for m in range(len(matrix[0])):
-#                         if matrix[row][m]!=0: matrix[row][m] = 'a'
-#                     for m in range(len(matrix)):
-#                         if matrix[m][col]!=0: matrix[m][col] = 'a'
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix[0])):
-#                 if matrix[i][j]=='a':
-#                     matrix[i][j] = 0
-
-# ##########################################################################################################
-
-# # T.C. -> O(N x M)
-# # S.C. -> O(N + M)
-
-# class Solution:
-#     def setZeroes(self, matrix: List[List[int]]) -> None:
-        
-#         r = ['a']*len(matrix)
-#         c = ['a']*len(matrix[0])
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix[0])):
-#                 if matrix[i][j]==0:
-#                     r[i]=0
-#                     c[j]=0
-#         for i in range(len(matrix)):
-#             for j in range(len(matrix[0])):
-#                 if r[i]==0 or c[j]==0:
-#                     matrix[i][j] = 0
-
-# ##########################################################################################################
-
-# # T.C. -> 2 x O(N x M)
-# # S.C. -> O(1)
-
-# class Solution:
-#     def setZeroes(self, matrix: List[List[int]]) -> None:
-#         col0 = 1
-#         row = len(matrix)
-#         col = len(matrix[0])
-#         for i in range(row):
-#             if matrix[i][0]==0: col0=0
-#             for j in range(1,col):
-#                 if matrix[i][j]==0:
-#                     matrix[i][0] = matrix[0][j] = 0
-#         for i in range(row-1,-1,-1):
-#             for j in range(col-1,0,-1):
-#                 if matrix[i][0]==0 or matrix[0][j]==0:
-#                     matrix[i][j] = 0
-#             if col0==0: matrix[i][0] = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        
-        m,n = len(matrix), len(matrix[0])
-        row = [None]*m
-        col = [None]*n
-        
+        m=len(matrix)
+        n=len(matrix[0])
+        temp=[["x"]*n for i in range(m)]
         for i in range(m):
             for j in range(n):
-                
                 if matrix[i][j]==0:
-                    row[i] = 0
-                    col[j] = 0
+                    row=i
+                    col=j
+                    for k in range(0,n):
+                        temp[row][k]=-1
+                    for k in range(0,m):
+                        temp[k][col]=-1
+                        
         for i in range(m):
             for j in range(n):
-                if row[i]==0:
-                    matrix[i][j] = 0
-                if col[j]==0:
-                    matrix[i][j] = 0
-                    
+                if temp[i][j]==-1:
+                    matrix[i][j]=0
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#####################################################################################
+                    
+                    
+                        
+                    

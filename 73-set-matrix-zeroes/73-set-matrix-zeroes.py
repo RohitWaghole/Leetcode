7df5a710ec365efbcdd9372137e0d73500c89bb3
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-05 11:21:43 +0530

    Time: 419 ms (5.01%), Space: 14.6 MB (90.68%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,25 +1,88 @@
+# 4 solution 
+
+# class Solution:
+#     def setZeroes(self, matrix: List[List[int]]) -> None:
+#         m=len(matrix)
+#         n=len(matrix[0])
+#         temp=[["x"]*n for i in range(m)]
+#         for i in range(m):
+#             for j in range(n):
+#                 if matrix[i][j]==0:
+#                     row=i
+#                     col=j
+#                     for k in range(0,n):
+#                         temp[row][k]=-1
+#                     for k in range(0,m):
+#                         temp[k][col]=-1
+                        
+#         for i in range(m):
+#             for j in range(n):
+#                 if temp[i][j]==-1:
+#                     matrix[i][j]=0
+                    
+        # Time= O(m*n+(m+n))
+        # Space= O(m*n)
+        
+#####################################################################################
+  
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m=len(matrix)
         n=len(matrix[0])
-        temp=[["x"]*n for i in range(m)]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]==0:
                     row=i
                     col=j
                     for k in range(0,n):
-                        temp[row][k]=-1
+                        if matrix[row][k]!=0:
+                            matrix[row][k]="*"
                     for k in range(0,m):
-                        temp[k][col]=-1
-                        
+                        if matrix[k][col]!=0:
+                            matrix[k][col]="*"
+                            
         for i in range(m):
             for j in range(n):
-                if temp[i][j]==-1:
+                if matrix[i][j]=="*":
                     matrix[i][j]=0
-        
-#####################################################################################
                     
+                    #####################################################################################
+                    
+                    
+                    
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
                     
                         
                     

ac3c8375ee6943ed2059d6f6bd14194cc01af0d8
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-11 22:49:26 +0530

    Time: 156 ms (6.07%), Space: 15.2 MB (25.39%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -41,25 +41,43 @@
 
 ######################################################################################
 
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         n = len(prices)
+#         dp = [[0]*2 for _ in range(n+1)]
+        
+#         dp[n][0] = dp[n][1] = 0
+        
+#         for ind in range(n-1,-1,-1):
+#             for buy in range(2):
+#                 profit = 0
+#                 if buy:
+#                     profit = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+#                 else:
+#                     profit = max( prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0])
+#                 dp[ind][buy] = profit
+#         return dp[0][1]
+        
+
+######################################################################################
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
+        curr = [0,0]
+        ahead = [0,0]
         n = len(prices)
-        dp = [[0]*2 for _ in range(n+1)]
-        
-        dp[n][0] = dp[n][1] = 0
-        
         for ind in range(n-1,-1,-1):
             for buy in range(2):
                 profit = 0
                 if buy:
-                    profit = max(-prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1])
+                    profit = max(-prices[ind] + ahead[0], 0 + ahead[1])
                 else:
-                    profit = max( prices[ind] + dp[ind+1][1], 0 + dp[ind+1][0])
-                dp[ind][buy] = profit
-        return dp[0][1]
-        
-        
+                    profit = max( prices[ind] + ahead[1], 0 + ahead[0])
+                curr[buy] = profit
+            ahead = curr
+        return ahead[1]
         
         
         

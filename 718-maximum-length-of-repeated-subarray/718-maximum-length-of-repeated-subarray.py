2b15ffa9b4760e22fc9172b9d680e5929f79bdeb
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-20 22:49:56 +0530

    Time: 5001 ms (63.20%), Space: 39.3 MB (50.31%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -15,19 +15,32 @@
     
 ##########################################################################################################################################
 
+# class Solution:
+#     def findLength(self, A: List[int], B: List[int]) -> int:
+#         def check(length):
+#             seen = set(tuple(A[i:i+length]) 
+#                        for i in range(len(A) - length + 1))
+#             return any(tuple(B[j:j+length]) in seen 
+#                        for j in range(len(B) - length + 1))
+
+#         lo, hi = 0, min(len(A), len(B)) + 1
+#         while lo < hi:
+#             mi = (lo + hi) // 2
+#             if check(mi):
+#                 lo = mi + 1
+#             else:
+#                 hi = mi
+#         return lo - 1
+
+    
+##########################################################################################################################################
+
 class Solution:
     def findLength(self, A: List[int], B: List[int]) -> int:
-        def check(length):
-            seen = set(tuple(A[i:i+length]) 
-                       for i in range(len(A) - length + 1))
-            return any(tuple(B[j:j+length]) in seen 
-                       for j in range(len(B) - length + 1))
-
-        lo, hi = 0, min(len(A), len(B)) + 1
-        while lo < hi:
-            mi = (lo + hi) // 2
-            if check(mi):
-                lo = mi + 1
-            else:
-                hi = mi
-        return lo - 1
+    
+        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+        for i in range(len(A) - 1, -1, -1):
+            for j in range(len(B) - 1, -1, -1):
+                if A[i] == B[j]:
+                    memo[i][j] = memo[i + 1][j + 1] + 1
+        return max(max(row) for row in memo)

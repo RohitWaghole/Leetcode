0476a08931506ed3f53ddc284db2ff01e2b3be45
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-24 10:50:38 +0530

    Time: 67 ms (63.75%), Space: 15.7 MB (52.98%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -4,25 +4,88 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], target: int) -> List[List[int]]:
+        
+#         self.res = []
+#         def f(root,target,ds,s):
+            
+#             if not root:
+#                 return False
+            
+#             ds.append(root.val)
+#             s += root.val
+            
+#             left = f(root.left,target,ds,s)
+#             right = f(root.right,target,ds,s)
+            
+#             if not left and not right:
+#                 if s==target:
+#                     self.res.append(ds[:])
+#             s -= ds.pop()
+#             return True
+#         f(root,target,[],0)
+#         return self.res
+
+
+
+
+
+
+
+
 class Solution:
     def pathSum(self, root: Optional[TreeNode], target: int) -> List[List[int]]:
         
         self.res = []
-        def f(root,target,ds,s):
-            
+        
+        def f(root,ds,s):
             if not root:
                 return False
             
             ds.append(root.val)
             s += root.val
+            left = f(root.left,ds,s)
+            right = f(root.right,ds,s)
             
-            left = f(root.left,target,ds,s)
-            right = f(root.right,target,ds,s)
-            
-            if not left and not right:
+            if not(left or right):
                 if s==target:
                     self.res.append(ds[:])
-            s -= ds.pop()
+            ds.pop()
+            s -= root.val
             return True
-        f(root,target,[],0)
+        
+        f(root,[],0)
         return self.res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

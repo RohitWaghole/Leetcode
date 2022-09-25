d8d067ada0dd69c0e03cacae39c74b471164c940
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-25 14:29:54 +0530

    Time: 447 ms (7.54%), Space: 15.5 MB (34.28%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,7 +1,18 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         major = len(nums)//2
+#         for i in nums:
+#             if nums.count(i)>major:
+#                 return i
+
+###################################################################################
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         nums.sort()
+#         return nums[len(nums)//2]
+
+##################################################################################
 
 # class Solution:
 #     def majorityElement(self, nums: List[int]) -> int:
@@ -9,3 +20,19 @@ class Solution:
 #         for key,val in d.items():
 #             if val>(len(nums)//2):
 #                 return key
+
+###################################################################################
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 0
+        element = None
+        
+        for i in nums:
+            if cnt == 0:
+                element = i
+            if element == i:
+                cnt += 1
+            else:
+                cnt -= 1
+        return element

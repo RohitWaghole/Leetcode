4c8f2e1fca82402ff4137873aa484dfecd1f3964
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-25 14:44:25 +0530

    Time: 227 ms (38.03%), Space: 15.4 MB (21.56%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,12 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        majority = len(nums)//3
         
-        d = Counter(nums)
-        result = []
+        major = len(nums)//3
+        res = []
         
-        for key,val in d.items():
-            if val>majority:
-                result.append(key)
+        for i in set(nums):
+            if nums.count(i)>major:
+                res.append(i)
+                
+        return res
+    
+############################################################################################
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+#         majority = len(nums)//3
         
-        return result
+#         d = Counter(nums)
+#         result = []
+        
+#         for key,val in d.items():
+#             if val>majority:
+#                 result.append(key)
+        
+#         return result

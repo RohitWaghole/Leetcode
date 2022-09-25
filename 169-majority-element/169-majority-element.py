46a2b27cd31b2e59dfef6ef46b1e65fd476b2585
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-25 14:14:45 +0530

    Time: 443 ms (8.08%), Space: 15.5 MB (85.68%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,6 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = Counter(nums)
-        for key,val in d.items():
-            if val>(len(nums)//2):
-                return key
+        nums.sort()
+        return nums[len(nums)//2]
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         d = Counter(nums)
+#         for key,val in d.items():
+#             if val>(len(nums)//2):
+#                 return key

85131a4474e9bb5ffae01910f9bb423c9e37c5b2
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-25 15:31:30 +0530

    Time: 188 ms (55.18%), Space: 13.9 MB (58.69%) - LeetHub

---
+++ 372-super-pow/372-super-pow.py
@@ -0,0 +1,28 @@
+# class Solution:
+#     def superPow(self, a: int, b: List[int]) -> int:
+#         b = int(''.join(str(i) for i in b))
+#         return pow(a,b)%1337
+
+#################################################################
+
+# class Solution:
+#     def superPow(self, a: int, b: List[int]) -> int:
+        
+#         b = int(''.join(str(i) for i in b))
+        
+#         ans = 1
+#         while b>0:
+#             if b%2==1:
+#                 ans *= a
+#                 b -= 1
+#             else:
+#                 a *= a
+#                 b //= 2
+#         return ans%1337
+
+
+#################################################################
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        return pow(a, int(''.join(map(str, b))), 1337)

5026158eea5e820bae476e355b21e15d2bb05743
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-25 15:06:31 +0530

    Time: 33 ms (91.84%), Space: 13.9 MB (68.26%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,3 +1,9 @@
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         return x**n
+
+########################################################################
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        return math.pow(x,n)

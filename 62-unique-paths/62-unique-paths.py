96463610a6916e1d8cf4a54a11868e3878d8c1cf
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-09-30 10:45:01 +0530

    Time: 59 ms (30.29%), Space: 14 MB (32.64%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -61,20 +61,83 @@
 
 #####################################################################################################
 
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+        
+#         dp = [0]*n
+#         dp[0] = 1
+#         for i in range(m):
+#             temp = [0]*n
+#             for j in range(n):
+#                 up,left = 0,0
+#                 up = dp[j]
+#                 if j>0: left = temp[j-1]
+                
+#                 temp[j] = up + left
+#             dp = temp
+#         return dp[-1]
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        dp = [0]*n
-        dp[0] = 1
-        for i in range(m):
-            temp = [0]*n
-            for j in range(n):
-                up,left = 0,0
-                up = dp[j]
-                if j>0: left = temp[j-1]
-                
-                temp[j] = up + left
-            dp = temp
-        return dp[-1]
+        dp = [[-1]*n for i in range(m)]
+        def f(i,j,dp):
+            
+            if i<0 or j<0:
+                return 0
+            if i==0 and j==0:
+                return 1
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            up = f(i-1,j,dp)
+            left = f(i,j-1,dp)
+            dp[i][j] = up + left
+            return dp[i][j]
+        
+        return f(m-1,n-1,dp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         

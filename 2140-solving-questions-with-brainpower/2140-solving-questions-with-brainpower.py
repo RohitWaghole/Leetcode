269605ea5ee0b7f4e2788cbafae53ed207b435ba
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-05 11:29:10 +0530

    Time: 3917 ms (16.94%), Space: 60.4 MB (70.40%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -10,16 +10,32 @@
 
 #####################################################################################################
 
+# class Solution:
+#     def mostPoints(self, questions: List[List[int]]) -> int:
+#         dp = [-1]*len(questions)
+#         def f(i,dp):
+#             if i>=len(questions):
+#                 return 0
+#             if dp[i]!=-1:
+#                 return dp[i]
+#             pick = questions[i][0] + f(i+questions[i][1]+1,dp)
+#             notpick = f(i+1,dp)
+#             dp[i] = max(pick, notpick)
+#             return dp[i]
+#         return f(0,dp)
+
+
+#####################################################################################################
+
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        dp = [-1]*len(questions)
-        def f(i,dp):
-            if i>=len(questions):
-                return 0
-            if dp[i]!=-1:
-                return dp[i]
-            pick = questions[i][0] + f(i+questions[i][1]+1,dp)
-            notpick = f(i+1,dp)
+        dp = [0]*len(questions)
+        for i in range(len(questions)-1,-1,-1):
+            pick = questions[i][0]
+            if i+questions[i][1]+1<len(questions):
+                pick += dp[i+questions[i][1]+1]
+            notpick = 0
+            if i+1<len(dp):
+                notpick += dp[i+1]
             dp[i] = max(pick, notpick)
-            return dp[i]
-        return f(0,dp)
+        return dp[0]

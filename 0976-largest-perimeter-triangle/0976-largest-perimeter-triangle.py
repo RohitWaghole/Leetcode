37ae622b39f7c112f75a37af606a175d46039e6d
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-12 21:39:20 +0530

    Time: 507 ms (12.91%), Space: 15.5 MB (45.85%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -1,6 +1,5 @@
 class Solution:
     def largestPerimeter(self, A: List[int]) -> int:
-        # classification , strain gage, potentiometer problems 
         A.sort()
         for i in range(len(A) - 3, -1, -1):
             if A[i] + A[i+1] > A[i+2]:

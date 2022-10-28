2be1bb91c7a0b7d8df607d8c554916bfcd58f89f
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-28 09:28:31 +0530

    Time: 239 ms (37.93%), Space: 19 MB (36.29%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        t = [[''.join(sorted(v)),i] for i,v in enumerate(strs)]
+        t.sort()
+        temp = []
+        res = []
+        tp = []
+        result = []
+        for i in range(len(t)):
+            if temp==[]:
+                temp.append(t[i])
+                tp.append(strs[t[i][1]])
+            elif temp[-1][0]==t[i][0]:
+                temp.append(t[i])
+                tp.append(strs[t[i][1]])
+            else:
+                res.append(temp)
+                result.append(tp)
+                tp = [strs[t[i][1]]]
+                temp = [t[i]]
+        if temp!=[]:
+            res.append(temp)
+            result.append(tp)
+        return result

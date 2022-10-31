318d36834d4ccdd533404af4cc032e5bd86c6860
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-31 21:25:06 +0530

    Time: 2420 ms (49.70%), Space: 25.1 MB (38.63%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,89 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         profit = 0
+#         for i in range(len(prices)):
+#             for j in range(i+1,len(prices)):
+#                 if prices[j]>prices[i]:
+#                     profit = max(profit, prices[j]-prices[i])
+#         return profit
+        
+###################################################################################
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         buy,sell = float('inf'),0
+#         for stock in prices:
+#             buy = min(buy, stock)
+#             sell = max(sell, stock - buy)
+#         return sell
+
+####################################################################################
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+        
+#         buy, sell = prices[0],0
+#         for stock in prices:
+#             if stock<buy:
+#                 buy = stock
+#             if stock - buy > sell:
+#                 sell = stock - buy
+#         return sell
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        mx,mn = 0,float('inf')
+        for i in prices:
+            mn = min(mn,i)
+            mx = max(mx,i-mn)
+        return mx
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

d1cf7201bf5443e0bd92edbc4ad0cc644d8aa0e7
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-31 21:11:13 +0530

    Time: 1690 ms (52.34%), Space: 27.9 MB (78.18%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,92 @@
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+        
+#         mx = sum(nums)
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 mx = max(mx,sum(nums[i:j+1]))
+#             mx = max(mx,nums[i])
+#         return mx
+
+#######################################################################################
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+        
+#         mx, s = nums[0],0
+#         for i in range(len(nums)):
+#             s += nums[i]
+#             mx = max(mx,s)
+#             if s<0:
+#                 s = 0
+#         return mx
+    
+# #################################################################################
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+        
+#         mx,mn = nums[0],nums[0]
+#         for i in range(1,len(nums)):
+#             mn = max(nums[i],nums[i] + mn)
+#             mx = max(mx, mn)
+#         return mx
+
+# ####################################################################################
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+        
+#         for i in range(1,len(nums)):
+#             if nums[i-1]>=0:
+#                 nums[i]+=nums[i-1]
+#         return max(nums)
+
+
+
+
+
+
+
+# ####################################################################################
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        for i in range(1,len(nums)):
+            if nums[i-1]>0:
+                nums[i]+=nums[i-1]
+        return max(nums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

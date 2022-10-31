2579510ab9f9fa3b04cff89ce6b77b4fd69d1acf
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-31 20:24:10 +0530

    Time: 134 ms (75.38%), Space: 13.8 MB (98.45%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,13 +1,26 @@
+# class Solution:
+#     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+#         d = {}
+#         for i in range(len(matrix[0])):
+#             d[0-i] = matrix[0][i]
+#         for i in range(len(matrix)):
+#             d[i] = matrix[i][0]
+            
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if matrix[i][j]!=d[i-j]:
+#                     return False
+#         return True
+
+####################################################################################################################
+
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         d = {}
-        for i in range(len(matrix[0])):
-            d[0-i] = matrix[0][i]
-        for i in range(len(matrix)):
-            d[i] = matrix[i][0]
-            
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j]!=d[i-j]:
+                if i-j not in d:
+                    d[i-j] = matrix[i][j]
+                elif matrix[i][j]!=d[i-j]:
                     return False
         return True

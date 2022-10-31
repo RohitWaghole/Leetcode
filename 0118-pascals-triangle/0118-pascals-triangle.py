0f08410aa97a05a3b5f63b7ed6d8f794baf09224
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-31 20:52:07 +0530

    Time: 55 ms (56.17%), Space: 13.9 MB (18.29%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,11 +4,11 @@
 class Solution:
     def generate(self, n: int) -> List[List[int]]:
         
-        p = [[1]]
+        pascal = [[1]]
         for i in range(n-1):
             temp = [1]
-            for j in range(len(p[-1])-1):
-                temp.append(p[-1][j]+p[-1][j+1])
+            for j in range(len(pascal[-1])-1):
+                temp.append(pascal[-1][j]+pascal[-1][j+1])
             temp.append(1)
-            p.append(temp)
-        return p
+            pascal.append(temp)
+        return pascal

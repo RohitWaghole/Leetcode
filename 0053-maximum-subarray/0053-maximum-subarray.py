38467f1f46d192bdd95cd54ca8cfa5679194fe1a
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-10-31 21:15:05 +0530

    Time: 1733 ms (48.40%), Space: 27.9 MB (40.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -34,61 +34,11 @@
 
 # ####################################################################################
 
-# class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-        
-#         for i in range(1,len(nums)):
-#             if nums[i-1]>=0:
-#                 nums[i]+=nums[i-1]
-#         return max(nums)
-
-
-
-
-
-
-
-# ####################################################################################
-
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        mx = nums[0]
-        s = 0
-        for i in nums:
-            s += i
-            mx = max(mx,s)
-            if s<0:
-                s = 0
-        return mx
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for i in range(1,len(nums)):
+            if nums[i-1]>=0:
+                nums[i]+=nums[i-1]
+        return max(nums)
 

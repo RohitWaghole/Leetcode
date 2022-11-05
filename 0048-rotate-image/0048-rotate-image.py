1adac3da2d2a5ddb0f3f7a72119dd6b4b88b2d4f
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-11-05 10:58:49 +0530

    Time: 62 ms (65.02%), Space: 14 MB (29.98%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -21,77 +21,21 @@
 # T.C. -> O(N^2) + O(N^2)
 # S.C. -> O(1)
 
-# class Solution:
-#     def rotate(self, matrix: List[List[int]]) -> None:
-        
-#         n = len(matrix)
-        
-#         for i in range(n):
-#             for j in range(0,i):
-#                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-        
-#         def rev(a):
-#             i,j = 0,len(a)-1
-#             while i<j:
-#                 a[i],a[j]=a[j],a[i]
-#                 i+=1
-#                 j-=1
-#             return a
-#         for i in range(n):
-#             matrix[i] = rev(matrix[i])
-
-
-
-
-
-
-
-
-
-
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         
         n = len(matrix)
         
         for i in range(n):
-            for j in range(i,n):
+            for j in range(0,i):
                 matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        def rev(a):
+            i,j = 0,len(a)-1
+            while i<j:
+                a[i],a[j]=a[j],a[i]
+                i+=1
+                j-=1
+            return a
         for i in range(n):
-            matrix[i] = matrix[i][::-1]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            matrix[i] = rev(matrix[i])

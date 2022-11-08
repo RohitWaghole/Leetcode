b6279cf8bd347b8ce7939bfa6ee444ee037a76e9
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-11-08 22:01:12 +0530

    Time: 77 ms (29.60%), Space: 13.8 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -22,13 +22,32 @@
 
 ###################################################################################################
 
+# class Solution:
+#     def makeGood(self, s: str) -> str:
+#         # If we find a pair in 's', remove this pair from 's'
+#         # and solve the remaining string recursively.
+#         for i in range(len(s) - 1):
+#             if abs(ord(s[i]) - ord(s[i + 1])) == 32:
+#                 return self.makeGood(s[:i] + s[i + 2:])
+        
+#         # Base case, if we can't find a pair, just return 's'.
+#         return s
+
+#######################################################################################################
+
 class Solution:
     def makeGood(self, s: str) -> str:
-        # If we find a pair in 's', remove this pair from 's'
-        # and solve the remaining string recursively.
-        for i in range(len(s) - 1):
-            if abs(ord(s[i]) - ord(s[i + 1])) == 32:
-                return self.makeGood(s[:i] + s[i + 2:])
+        # Use stack to store the visited characters.
+        stack = []
         
-        # Base case, if we can't find a pair, just return 's'.
-        return s
+        # Iterate over 's'.
+        for curr_char in list(s):
+            # If the current character make a pair with the last character in the stack,
+            # remove both of them. Otherwise, we add the current character to stack.
+            if stack and abs(ord(curr_char) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(curr_char)
+        
+        # Returns the string concatenated by all characters left in the stack.
+        return "".join(stack)

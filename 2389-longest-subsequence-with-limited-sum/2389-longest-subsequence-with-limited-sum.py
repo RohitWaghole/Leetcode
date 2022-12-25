0b5bd1896703de24f3f4453c5eaadedafaa6588b
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2022-12-25 11:27:21 +0530

    Time: 101 ms (97.86%), Space: 14.2 MB (79.61%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,14 +1,32 @@
+# class Solution:
+#     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+#         nums.sort()
+#         ans = []
+#         for q in queries:
+#             c = 0
+#             for i in nums:
+#                 if q>=i:
+#                     q -= i
+#                     c += 1
+#                 else:
+#                     break
+#             ans.append(c)
+#         return ans
+
+##############################################################################################
+
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        # Get the prefix sum array of the sorted 'nums'.
         nums.sort()
-        ans = []
-        for q in queries:
-            c = 0
-            for i in nums:
-                if q>=i:
-                    q -= i
-                    c += 1
-                else:
-                    break
-            ans.append(c)
-        return ans
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        
+        answer = []
+        
+        # For each query, find its insertion index to the prefix sum array.
+        for query in queries:
+            index = bisect.bisect_right(nums, query)
+            answer.append(index)
+            
+        return answer

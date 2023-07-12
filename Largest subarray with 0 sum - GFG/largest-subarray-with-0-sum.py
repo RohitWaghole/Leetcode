7acd4f88ea3fb1465bad2cdf73fa7c3a1d5d7b53
Rohit Waghole <68462227+RohitWaghole@users.noreply.github.com>
2023-07-12 13:43:08 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.py
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.py
@@ -2,46 +2,26 @@
 #Your should return the required output
 class Solution:
     def maxLen(self, n, arr):
-        #Code here
-        hash_map = {}
-        max_length = 0
-        curr_sum = 0
-        
-        for index,value in enumerate(arr):
+        d = {}
+        s = 0
+        mx = 0
+        k = 0
+        for i in range(n):
+            s += arr[i]
+            if s==0:
+                mx = max(mx, i+1)
+            if d.get(s-k,'a')!='a':
+                j = d[s-k]
+                l = i-j
+                mx = max(mx, l)
             
-            curr_sum += value
-            if curr_sum == 0:
-                max_length = index+1
-            
-            if value==0 and max_length == 0:
-                max_length = 1
-                
-            if curr_sum in hash_map:
-                
-                max_length = max(max_length, index - hash_map[curr_sum])
-                
-            else:
-                
-                hash_map[curr_sum] = index
-                
-        return max_length
-        
-        
-        
-        
-        
-        #TIME LIMIT EXCEED
-        # mx = 0
-        # for i in range(n):
-        #     s = 0
-        #     for j in range(i,n):
-        #         s+=arr[j]
-        #         if s==0:
-        #             mx = max(mx,j-i+1)
-        # return mx
+            if d.get(s,'a')=='a':
+                d[s] = i
+        return mx
+
 
 #{ 
-#  Driver Code Starts
+ # Driver Code Starts
 if __name__=='__main__':
     t= int(input())
     for i in range(t):
